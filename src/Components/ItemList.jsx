8f7d15a9b2a05671a869293aebf58aff32cc93e6
mohammedsuhail85 <suhailmsm@gmail.com>
2020-11-26 23:31:49 +0530

    Added React-Query and Modifications

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -11,54 +11,74 @@ import {
   theme,
   Heading,
   GridItem,
+  CircularProgress,
+  Center,
 } from '@chakra-ui/react';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 import Item from "./Item";
 import AddItemModel from "./AddItem";
+import NotAvailable from './NotAvailable'
+
+const fetchProperties = async () => {
+  const res = await fetch('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/fetchallitems');
+  return res.json();
+}
 
 export default function ItemList() {
 
   const [hasError, setErrors] = useState(false);
   const [listProperty, setListProperty] = useState([]);
 
-  useEffect(async () => {
-    axios.get('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/fetchallitems', 
-    {headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }})
-    .then(response => {
-      console.log(response.data);
-      setListProperty(response.data);
-    }, error => {
-      console.log(error);
-    });
+  const { data, status } = useQuery('properties', fetchProperties);
 
-  }, []);
+  // useEffect(async () => {
+  //   await axios.get('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/fetchallitems', 
+  //   {headers : { 
+  //     'Content-Type': 'application/json',
+  //     'Accept': 'application/json'
+  //    }})
+  //   .then(response => {
+  //     console.log(response.data);
+  //     setListProperty(response.data);
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }, []);
 
   return (
     <div>
-
-      <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-        <GridItem textAlign="left" colSpan={2} h="10">
-          <Heading as="h3" size="lg">List of available items</Heading>
-        </GridItem>
-        <GridItem colStart={5} textAlign="right" colEnd={6} h="10"><AddItemModel /></GridItem>
-      </Grid>
-
-      <Box textAlign="left" fontSize="xl">
-
-        <br />
-        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-          {
-            listProperty.map(property => {
-              return <Item key={property.id} property={property} />
-            })
-          }
-        </Grid>
-
-      </Box>
+      {status === 'error' && (
+        <div>
+          <NotAvailable />
+        </div>
+      )}
+      {status === 'loading' && (
+        <Center p="20">
+          <CircularProgress isIndeterminate color="green.300" />
+        </Center>
+      )}
+      {status === 'success' && (
+        <div>
+          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+            <GridItem textAlign="left" colSpan={2} h="10">
+              <Heading as="h3" size="lg">List of available items</Heading>
+            </GridItem>
+            <GridItem colStart={5} textAlign="right" colEnd={6} h="10"><AddItemModel /></GridItem>
+          </Grid>
+          <Box textAlign="left" fontSize="xl">
+            <br />
+            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+              {
+                data.map(property => {
+                  return <Item key={property.id} property={property} />
+                })
+              }
+            </Grid>
+          </Box>
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/ViewItem.jsx
+++ src/Components/ViewItem.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import {
   Box,
   Text,
@@ -15,149 +14,154 @@ import {
   NumberDecrementStepper,
   HStack,
   Button,
+  CircularProgress,
 } from '@chakra-ui/react';
 import {
   useParams
 } from "react-router-dom";
-import axios from 'axios';
+import { useQuery } from 'react-query';
 
 import NotAvailable from './NotAvailable';
 
+const fetchItem = async (key, id) => {
+  const res = await fetch('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/getpropertybyId/' + id);
+  return res.json();
+}
+
 export default function ViewItem() {
-
   let { id } = useParams();
+  const { data, status } = useQuery(['item', id], fetchItem);
 
-  const [hasError, setErrors] = useState(false);
-  const [property, setProperty] = useState({});
+  // useEffect(async () => {
+  //   await axios.get('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/getpropertybyId/' + id,
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Accept': 'application/json'
+  //       }
+  //     })
+  //     .then(response => {
+  //       console.log(response.data);
+  //       setProperty(response.data);
+  //     }, error => {
+  //       console.log(error);
+  //       setErrors(true);
+  //     });
 
-  useEffect(async () => {
-    await axios.get('https://extendsclass.com/mock/rest/6861d7c0f53686808c4c50d53259092e/getpropertybyId/' + id,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      })
-      .then(response => {
-        console.log(response.data);
-        setProperty(response.data);
-      }, error => {
-        console.log(error);
-        setErrors(true);
-      });
-
-  }, [property]);
-
-  if (hasError) {
-    return <NotAvailable />
-  } else {
-    return (
-      <div>
-        <br />
-        <br />
-        <Center>
-          <Box textAlign="left" fontSize="xl" p={15} w="80%" >
-            <div>
+  // }, [property]);
+  return (
+    <div>
+      {status === 'error' && (
+        <div>
+          <NotAvailable />
+        </div>
+      )}
+      {status === 'loading' && (
+        <Center p="20">
+          <CircularProgress isIndeterminate color="green.300" />
+        </Center>
+      )}
+      {status === 'success' && (
+        <div>
+          <br />
+          <br />
+          <Center>
+            <Box textAlign="left" fontSize="xl" p={15} w="80%" >
+              <div>
+                <br />
+                <Heading as="h3" size="lg" textAlign="">
+                  {data.title}
+                </Heading>
+              </div>
               <br />
-              <Heading as="h3" size="lg" textAlign="">
-                {property.title}
-              </Heading>
-            </div>
-            <br />
-            <br />
-            <div>
-              <Flex>
-                <Box
-                  d="flex"
-                  position="relative"
-                  h="425px"
-                  w="600px"
-                  borderWidth="1px"
-                  borderRadius="lg"
-                  overflow="hidden"
-                >
-                  <Image src={property.imageUrl} alt={property.imageAlt} h="100%" w="100%" />
-                </Box>
+              <br />
+              <div>
+                <Flex>
+                  <Box
+                    d="flex"
+                    position="relative"
+                    h="425px"
+                    w="600px"
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    overflow="hidden"
+                  >
+                    <Image src={data.imageUrl} alt={data.imageAlt} h="100%" w="100%" />
+                  </Box>
 
-                {/* <Square color="transparent" size="150px">
+                  {/* <Square color="transparent" size="150px">
               </Square> */}
 
-                <Box flex="1">
-                  <Box p="10">
-                    <Box d="flex" alignItems="baseline">
-                      <Badge borderRadius="full" px="2" colorScheme="teal">
-                        New
+                  <Box flex="1">
+                    <Box p="10">
+                      <Box d="flex" alignItems="baseline">
+                        <Badge borderRadius="full" px="2" colorScheme="teal">
+                          New
                   </Badge>
-                      <Box
-                        color="gray.500"
-                        fontWeight="semibold"
-                        letterSpacing="wide"
-                        fontSize="xs"
-                        textTransform="uppercase"
-                        ml="2"
-                      >
-                        {property.beds} beds &bull; {property.baths} baths
+                        <Box
+                          color="gray.500"
+                          fontWeight="semibold"
+                          letterSpacing="wide"
+                          fontSize="xs"
+                          textTransform="uppercase"
+                          ml="2"
+                        >
+                          {data.beds} beds &bull; {data.baths} baths
                   </Box>
-                    </Box>
-                    <Box
-                      mt="1"
-                      fontWeight="semibold"
-                      as="h4"
-                      lineHeight="tight"
-                      isTruncated
-                    >
-                      <Box as="span" color="grey" fontSize="2xl">
-                        {property.title}
                       </Box>
-                    </Box>
-                    <Box color="grey">
-                      {property.formattedPrice}
-                      <Box as="span" color="gray.600" fontSize="sm">
-                        / wk
+                      <Box
+                        mt="1"
+                        fontWeight="semibold"
+                        as="h4"
+                        lineHeight="tight"
+                        isTruncated
+                      >
+                        <Box as="span" color="grey" fontSize="2xl">
+                          {data.title}
+                        </Box>
+                      </Box>
+                      <Box color="grey">
+                        {data.formattedPrice}
+                        <Box as="span" color="gray.600" fontSize="sm">
+                          / wk
                   </Box>
-                    </Box>
-                    <Box d="flex" mt="2" alignItems="center">
-                      <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                        {property.reviewCount} reviews
+                      </Box>
+                      <Box d="flex" mt="2" alignItems="center">
+                        <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                          {data.reviewCount} reviews
                   </Box>
-                    </Box>
-                    <Text fontSize="lg" color="grey">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quis! Repellat nemo reiciendis veniam possimus odit dolores,
-                      inventore ipsa labore, odio minima eius ipsam sequi facilis perspiciatis ab voluptatibus tempore!
+                      </Box>
+                      <Text fontSize="lg" color="grey">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quis! Repellat nemo reiciendis veniam possimus odit dolores,
+                        inventore ipsa labore, odio minima eius ipsam sequi facilis perspiciatis ab voluptatibus tempore!
                   </Text>
-                    <Text color="grey" fontSize="lg">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quis! Repellat nemo reiciendis veniam possimus odit dolores,
-                      inventore ipsa labore, odio minima eius ipsam sequi facilis perspiciatis ab voluptatibus tempore!
+                      <Text color="grey" fontSize="lg">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quis! Repellat nemo reiciendis veniam possimus odit dolores,
+                        inventore ipsa labore, odio minima eius ipsam sequi facilis perspiciatis ab voluptatibus tempore!
                   </Text>
-                    <br />
-                    <HStack>
-                      <NumberInput defaultValue={1} min={1} max={20} w="150px" color="grey">
-                        <NumberInputField />
-                        <NumberInputStepper>
-                          <NumberIncrementStepper />
-                          <NumberDecrementStepper />
-                        </NumberInputStepper>
-                      </NumberInput>
+                      <br />
+                      <HStack>
+                        <NumberInput defaultValue={1} min={1} max={20} w="150px" color="grey">
+                          <NumberInputField />
+                          <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                          </NumberInputStepper>
+                        </NumberInput>
 
-                      <Button colorScheme="teal" variant="solid">
-                        Set Booking
+                        <Button colorScheme="teal" variant="solid">
+                          Set Booking
                     </Button>
-                    </HStack>
+                      </HStack>
+                    </Box>
                   </Box>
-                </Box>
-              </Flex>
+                </Flex>
 
-            </div>
+              </div>
 
-          </Box>
-        </Center>
-      </div>
-    );
-  }
-
-  ReactDOM.render(
-    // Try changing to isLoggedIn={true}:
-    <ViewItem />,
-    document.getElementById('root')
+            </Box>
+          </Center>
+        </div>)}
+    </div>
   );
 }
